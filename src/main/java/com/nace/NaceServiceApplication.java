54f639a82915ab4c38f48198d13affb6e6b33dd9
Siddharth Vaidya <vaidyasiddharth17@gmail.com>
2021-11-29 08:01:29 +0530

    commit

--- src/main/java/com/nace/NaceServiceApplication.java
+++ src/main/java/com/nace/NaceServiceApplication.java
@@ -1,8 +1,12 @@
 package com.nace;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -10,9 +14,11 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @SpringBootApplication
 @EnableSwagger2
-public class NaceServiceApplication {
+public class NaceServiceApplication extends WebMvcConfigurationSupport {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NaceServiceApplication.class, args);
@@ -31,12 +37,19 @@ public class NaceServiceApplication {
 				.apiInfo(apiInfo());
 
 
+
 	}
-	public ApiInfo apiInfo() {
+	private ApiInfo apiInfo() {
 		return new ApiInfo(" Nace Service", "Simple Micro service", "0.0.1",
 				"mail@mail.com", "mail@mail.com", " ", " ");
 
 	}
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+
 
 
 
